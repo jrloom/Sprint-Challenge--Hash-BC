c9dabcef019d0e02c5a5d1d144891ae3736d0d82
Jason Loomis <jason@jrloomis.com>
2020-04-10 11:44:03 -0400

    miner is mining

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -23,8 +23,13 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
+    # proof = 0
+    proof = random.randint(0, 1000)
     #  TODO: Your code here
+    previous_hash = hashlib.sha256(f'{last_proof}'.encode()).hexdigest()
+
+    while not valid_proof(previous_hash, proof):
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,7 +45,9 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    current_hash = hashlib.sha256(f'{proof}'.encode()).hexdigest()
+
+    return last_hash[-6:] == current_hash[:6]
 
 
 if __name__ == "__main__":
